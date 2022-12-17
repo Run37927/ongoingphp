c9cda669bd0e67075b8f769ba529dbc3638a689e
Hairun Huang <hairunhuang@gmail.com>
2022-12-17 14:55:28 -0800

    heredoc

--- stringType.php
+++ stringType.php
@@ -7,8 +7,17 @@ $name = $first . ' ' . $last;
 
 echo $name . '<br>';
 
-// echo $name[-1]; // h
+echo $name[-1] . '<br>'; // h
 
 $name[1] = 'a';
 
-echo $name;
+echo $name . '<br>';
+
+// Heredoc
+$text = <<<TEXT
+line 1
+line 2
+line 3
+TEXT;
+
+echo nl2br($text);
