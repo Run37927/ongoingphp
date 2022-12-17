40891f211f99d18f8f1a42c60ccd779089af6de1
Hairun Huang <hairunhuang@gmail.com>
2022-12-17 15:12:23 -0800

    Update arrayType.php

--- arrayType.php
+++ arrayType.php
@@ -3,5 +3,12 @@
 $programmingLanguages = ['php', 'java', 'python'];
 
 echo $programmingLanguages[1]; // java
+echo $programmingLanguages[-1]; // undefined key
+echo $programmingLanguages[3]; // undefined key
 
-echo $programmingLanguages[-1]; // undefined 
+var_dump(isset($programmingLanguages[3])); // bool(false) meaning it doesn't exist
+var_dump(isset($programmingLanguages[1])); // bool(true)
+
+$programmingLanguages[1] = 'C';
+
+echo $programmingLanguages[1];
