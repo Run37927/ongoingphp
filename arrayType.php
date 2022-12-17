b97f757b8d97a5ecc93ac069bf56a9c398cf2d37
Hairun Huang <hairunhuang@gmail.com>
2022-12-17 15:18:55 -0800

    use array_push()

--- arrayType.php
+++ arrayType.php
@@ -13,16 +13,20 @@ $programmingLanguages[1] = 'C';
 
 echo $programmingLanguages[1]; // C
 
-echo '<br>';
+
 echo '<pre>';
 print_r($programmingLanguages);
 echo '</pre>';
 echo count($programmingLanguages); // 3 meanings 3 elements in the array
 
 
-echo '<br>';
 $programmingLanguages[] = 'C++'; // will just push to the end of the array
 echo '<pre>';
 print_r($programmingLanguages);
 echo '</pre>';
 echo count($programmingLanguages); // 4
+
+array_push($programmingLanguages, 'Elixir', 'Ocaml', 'Rust');
+echo '<pre>';
+print_r($programmingLanguages);
+echo '</pre>';
