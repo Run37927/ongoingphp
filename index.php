defaaaa401f91863ce7fb42cf2979712e9b95bb1
Hairun Huang <hairunhuang@gmail.com>
2022-12-17 12:44:42 -0800

    embed php in html

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
-<?php
-// echo print 'hello world'; // print will return 1
-// echo 'hello', ' con ', 'world';
-$name = 'dan';
-echo $name;
-
-echo "joe's invoice\n";
-echo 'john\'s invoice';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php echo 'hello world' ?>
+    </h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel illo, maxime libero accusantium quae exercitationem facilis optio temporibus laboriosam enim necessitatibus magni, obcaecati ab? At, eaque! Dicta libero ex rerum.</p>
+</body>
+</html>
