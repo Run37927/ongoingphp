2f5eab380ae754537da12db2adc9f6d55440c570
Hairun Huang <hairunhuang@gmail.com>
2022-12-17 13:56:49 -0800

    hexdecimal

--- integerType.php
+++ integerType.php
@@ -3,3 +3,12 @@
 echo 'max int = ' . PHP_INT_MAX . '<br>';
 echo 'min int = ' . PHP_INT_MIN . '<br>';
 echo 'int size = ' . PHP_INT_SIZE . '<br>';
+
+# output
+# max int = 9223372036854775807
+# min int = -9223372036854775808
+# int size = 8
+
+$x = 0x2A;
+
+echo $x;
