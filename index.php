9febc356e8d2aec73405e577133042a81ba42006
Hairun Huang <hairunhuang@gmail.com>
2022-12-17 13:17:02 -0800

    boolean false prints blank

--- index.php
+++ index.php
@@ -7,11 +7,13 @@
     # string
 
 $completed = true;
+$paid = false;
 $score = 88;
 $price = 0.99;
 $greeting = 'hello john';
 
 echo $completed . '<br>';
+echo $paid  . '<br>';
 echo $score . '<br>';
 echo $price . '<br>';
 echo $greeting . '<br>';
