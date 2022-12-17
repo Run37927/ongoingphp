de518085d8af2b1cebc3659bdf99ac1a3afcba31
Hairun Huang <hairunhuang@gmail.com>
2022-12-17 13:01:37 -0800

    weird way of defining constants but ok

--- index.php
+++ index.php
@@ -1,32 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>
-        heading here
-    </h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel illo, maxime libero accusantium quae exercitationem facilis optio temporibus laboriosam enim necessitatibus magni, obcaecati ab? At, eaque! Dicta libero ex rerum.</p>
+<?php
 
-    <?php
-        // comment 1
-        # comment 2
+//constants
 
-        /*
-        multi line comment
-        */
+define('STATUS_PAID', 'paid');
 
-        /**
-         * document comment, kinda like javadocs
-         */
-        $x = 10;
-        $y = 45;
-
-        echo '<h2>' . $x . ', ' , $y . '</h2>';
-    ?>
-</body>
-</html>
+echo STATUS_PAID;
