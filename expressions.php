488d848ab994cc966768d053298acef22b5a1b35
Hairun Huang <hairunhuang@gmail.com>
2022-12-17 16:33:14 -0800

    Create expressions.php

---
+++ expressions.php
@@ -0,0 +1,13 @@
+<?php
+
+//  almost everything in PHP is expression
+
+$x = 5; // an expression
+
+$y = $x; // an expression
+
+$z = $x === $y; // an expression
+
+$z = sum($x, $y); // an expression
+
+
