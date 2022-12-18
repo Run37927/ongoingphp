94ca6d74afe39f4b18ba445e256567f8b2c789aa
Hairun Huang <hairunhuang@gmail.com>
2022-12-17 16:42:11 -0800

    Update operators.php

--- operators.php
+++ operators.php
@@ -1,11 +1,17 @@
 <?php
 
 $x = 10;
-$y = 2;
+$y = 3;
 
-echo $x / $y . '<br>'; // 5
-echo $x ** $y . '<br>'; // 100
+var_dump($x / $y);echo '<br>'; // float(3.3333333333333335)
+var_dump($x % $y);echo '<br>'; // int(1)
 
 $x = '10';
 var_dump($x); echo '<br>'; // string(2) "10"
 var_dump(+$x); // int(10)
+
+$a = 9;
+$b = 0;
+
+// var_dump($a / $b); Uncaught DivisionByZeroError: Division by zero in
+var_dump(fdiv($a, $b)); //float(INF)
