d7bd06d57b31d770d52d2980f706206fac3880fb
Hairun Huang <hairunhuang@gmail.com>
2022-12-17 17:08:36 -0800

    terinary operator

--- operators.php
+++ operators.php
@@ -63,3 +63,22 @@ echo '<br>';
 var_dump($num1 >= $num2); // true
 echo '<br>';
 var_dump($num1 <=> $num2); // 0 if two are equal, -1 if num1 < num2, 1 if num1> num2
+echo '<br>';
+
+
+$var1 = 'Hello There';
+$var2 = strpos($var1, 'H');
+var_dump($var2); echo '<br>'; // int(0)
+
+if($y == false) {
+    echo 'H not found';
+} else {
+    echo 'H found at index ' . $var2;
+}
+
+echo '<br>';
+$result = $y === false ? 'H not found' : 'H found at index ' . $var2;
+
+echo $result;
+#================error control operators=======================
+echo '<br>' . '========error control operators=======' . '<br>';
