822d570d4a301ce79989b687f463ca61b2f3f41c
Hairun Huang <hairunhuang@gmail.com>
2022-12-17 16:46:25 -0800

    Update operators.php

--- operators.php
+++ operators.php
@@ -8,10 +8,19 @@ var_dump($x % $y);echo '<br>'; // int(1)
 
 $x = '10';
 var_dump($x); echo '<br>'; // string(2) "10"
-var_dump(+$x); // int(10)
+var_dump(+$x);echo '<br>';  // int(10)
 
 $a = 9;
 $b = 0;
 
 // var_dump($a / $b); Uncaught DivisionByZeroError: Division by zero in
-var_dump(fdiv($a, $b)); //float(INF)
+var_dump(fdiv($a, $b));echo '<br>';  //float(INF)
+
+
+#================assignment=======================
+echo '<br>' . '========assignment=======' . '<br>';
+$p = $q = 10;
+var_dump($p, $q);echo '<br>'; 
+
+$p = ($q = 10) + 5;
+var_dump($p, $q);echo '<br>'; // int(15) int(10)
