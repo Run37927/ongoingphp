432518c748f0e9809e11589121eb237900942806
Hairun Huang <hairunhuang@gmail.com>
2022-12-18 22:49:33 -0800

    another way to write while loop

--- loops.php
+++ loops.php
@@ -1,9 +1,18 @@
 <?php
 
 $i = 0;
-while (true) {
-    while ($i > 10) {
-        break 2;
+// while (true) {
+//     while ($i > 10) {
+//         break 2;
+//     }
+//     echo $i++ . '<br>';
+// }
+
+
+while ($i <= 15):
+    if ($i % 2 == 0) {
+        $i++;
+        continue;
     }
     echo $i++ . '<br>';
-}
+endwhile;
