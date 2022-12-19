ee137aad39914d81c043994f89f810dbea808ae2
Hairun Huang <hairunhuang@gmail.com>
2022-12-18 22:55:19 -0800

    Update loops.php

--- loops.php
+++ loops.php
@@ -17,14 +17,23 @@ $i = 0;
 //     echo $i++ . '<br>';
 // endwhile;
 
+
 // do {
 //     echo $i++ . ' ';
 // } while ($i <= 15);
+
 
 // for ($i; $i < 5; $i++) {
 //     echo $i . ' ';
 // }
 
-for ($i; $i < 5; print $i, $i++) {
-    // echo $i . ' ';
+
+// for ($i; $i < 5; print $i, $i++) {
+//     // echo $i . ' ';
+// }
+
+
+$text = 'jello world';
+for ($i; $i < strlen($text); $i++) {
+    echo $text[$i] . '<br>';
 }
