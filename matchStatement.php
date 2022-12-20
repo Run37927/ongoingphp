7ed6809fa13427274342d40112dad387639c1890
Hairun Huang <hairunhuang@gmail.com>
2022-12-19 17:04:26 -0800

    Update matchStatement.php

--- matchStatement.php
+++ matchStatement.php
@@ -1,14 +1,14 @@
 <?php
 // introduced in PHP 8
 
-$paymentStatus = 1;
+$paymentStatus = 3;
 
 
 // match is a expression, thus evaluates to a value
 $paymentStatusDisplay = match ($paymentStatus) {
     1 =>  'paid',
-    2 =>  'payment declined',
-    3 =>  'pending payment',
+    2,3 =>  'payment declined', // 2 or 3 will print payment declined
+    0 =>  'pending payment',
 };
 
 echo $paymentStatusDisplay;
