d4a7837acf8871178af36a486e9e2e6faecc750a
Hairun Huang <hairunhuang@gmail.com>
2022-12-19 16:59:14 -0800

    Update switchStatement.php

--- switchStatement.php
+++ switchStatement.php
@@ -19,3 +19,28 @@ switch($paymentStatus) {
     default:
         echo 'unknown payment status';
 }
+
+
+function func() {
+    sleep(3);
+    echo 'done <br>';
+    return 3;
+}
+
+// slightly faster than if statement
+switch(func()) {
+    case 1:
+        echo 1;
+        break;
+    
+    case 2:
+        echo 2;
+        break;
+    
+    case 3:
+        echo 3;
+        break;
+    
+    default:
+        echo 4;
+}
